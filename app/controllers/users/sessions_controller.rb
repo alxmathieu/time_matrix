fcf09e86aa6e35345753e7c194ee63d48e70003e
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2022-04-04 13:22:26 +0200

    destroy session method

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,4 +8,12 @@ class Users::SessionsController < ApplicationController
   def after_sign_in_path_for(resource_or_scope)
     stored_location_for(resource_or_scope) || root_path
   end
+
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    respond_to_on_destroy
+  end
+
 end
