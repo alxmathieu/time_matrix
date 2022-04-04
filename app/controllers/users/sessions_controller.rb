20af2a32536bffe24c99c748835627018b9f52f6
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2022-04-04 13:32:31 +0200

    sessions controller inherit from devise

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,4 @@
-class Users::SessionsController < ApplicationController
+class Users::SessionsController < Devise::SessionsController
   skip_before_action :authenticate_user!, only: [:new]
 
   def after_sign_out_path_for(_resource_or_scope)
@@ -9,11 +9,5 @@ class Users::SessionsController < ApplicationController
     stored_location_for(resource_or_scope) || root_path
   end
 
-  def destroy
-    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message! :notice, :signed_out if signed_out
-    yield if block_given?
-    respond_to_on_destroy
-  end
 
 end
